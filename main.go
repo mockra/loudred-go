5752ebdc5749eebca1fd6fd2797a6dfe81c71689
David Ratajczak <david@mockra.com>
2014-10-08 23:21:37 -0700

    adds twitter credentials for anaconda

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
   "github.com/ChimeraCoder/anaconda"
+  "os"
 )
 
 func main() {
+  anaconda.SetConsumerKey(os.Getenv("LR_CONSUMER_KEY"))
+  anaconda.SetConsumerSecret(os.Getenv("LR_CONSUMER_SECRET"))
+  api := anaconda.NewTwitterApi(os.Getenv("LR_ACCESS_TOKEN"), os.Getenv("LR_TOKEN_SECRET"))
 }
